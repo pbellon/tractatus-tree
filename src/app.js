cb2963d17fa225ccb50ad18ab832f744093b73cc
pbellon <bellon.pierre@gmail.com>
2014-09-29 19:38:44 +0200

    Massive update

--- src/app.js
+++ src/app.js
@@ -15,6 +15,22 @@ var tractatus = angular.module('tractatus-tree', [
     ]);
 
 
-tractatus.controller('MainCtrl', ['$scope', function($scope){
-    console.log("MainCtrl.init");
+tractatus.controller('MainCtrl', ['$scope', '$sce', 'constant.events', function($scope, $sce, EVENTS){
+    $scope.selectedNode = undefined;
+
+    $scope.$on(EVENTS.node.selected, function(evt, node){
+        node.selectedLanguage = 'en';
+        $scope.$apply(function(){
+            $scope.selectedNode = node;
+        });
+    }); 
+
+    $scope.changeLang = function(lang){
+        $scope.selectedNode.selectedLanguage = lang;
+    }
+
+    $scope.getContent = function(){
+        node = $scope.selectedNode
+        return $sce.trustAsHtml(node.content[node.selectedLanguage]);
+    }
 }]);
