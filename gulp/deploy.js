f42bb92a9929ed9b9a9f5a07123f0450145ead6f
pbellon <bellon.pierre@gmail.com>
2014-10-08 19:50:46 +0200

    Added gulp-deploy

---
+++ gulp/deploy.js
@@ -0,0 +1,9 @@
+var deploy = require('gulp-gh-pages');
+var gulp = require('gulp');
+
+gulp.task('deploy', function () {
+    return gulp.src('./dist/**/*')
+        .pipe(deploy({
+            
+        }));
+});
